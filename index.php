0b396d83bf3e61c6e4e228acb89bb8ef5523d87b
andredisa <andreadisanti22@gmail.com>
2025-04-07 13:00:01 +0200

    modificato la directory del form

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h2>Carica il file Excel da modificare</h2>
-    <form action="modifica_excel.php" method="POST" enctype="multipart/form-data">
+    <form action="backEnd/modifica_excel.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="fileExcel" accept=".xlsx, .xls" required>
         <br><br>
         <input type="submit" value="Carica e Modifica">
