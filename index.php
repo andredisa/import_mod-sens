9d894ae81f28b278e73f1d3cfa473916884ecbed
andredisa <andreadisanti22@gmail.com>
2025-04-07 12:06:05 +0200

    aggiunto un index che consente all'utente di caricare l'excel da modificare

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carica e modifica file Excel</title>
+</head>
+<body>
+    <h2>Carica il file Excel da modificare</h2>
+    <form action="modifica_excel.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="fileExcel" accept=".xlsx, .xls" required>
+        <br><br>
+        <input type="submit" value="Carica e Modifica">
+    </form>
+</body>
+</html>
