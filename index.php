90c0ae334fba836da2e2cc8449676472f39ba937
andredisa <andreadisanti22@gmail.com>
2025-04-08 08:32:20 +0200

    modificato l'index

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carica e modifica file Excel</title>
 </head>
+
 <body>
-    <h2>Carica il file Excel da modificare</h2>
+    <h2>Carica il file Excel e i due CSV (Sensori e Moduli)</h2>
     <form action="backEnd/modifica_excel.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="fileExcel" accept=".xlsx, .xls" required>
+        <label>Excel:</label><br>
+        <input type="file" name="fileExcel" accept=".xlsx,.xls" required><br><br>
+
+        <label for="sensoriFile">Carica il file dei sensori CSV:</label>
+        <input type="file" name="sensoriFile" accept=".csv" required>
         <br><br>
+        
+        <label for="moduliFile">Carica il file dei moduli CSV:</label>
+        <input type="file" name="moduliFile" accept=".csv" required>
+        <br><br>
+
         <input type="submit" value="Carica e Modifica">
     </form>
+
 </body>
+
 </html>
