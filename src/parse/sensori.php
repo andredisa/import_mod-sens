0bd2716ba0cf591d70da4cd5174d85e8e99d9302
andredisa <andreadisanti22@gmail.com>
2025-04-28 08:37:37 +0200

    milgiorato il parse per i sensori, 1.1, 1.2 etc

--- src/parse/sensori.php
+++ src/parse/sensori.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../gestione/funzioni.php';
-
-function parseSensors($csvFile) {
+function parseSensors($csvFile)
+{
     $rows = [];
 
     if (($handle = fopen($csvFile, "r")) !== FALSE) {
@@ -9,15 +9,20 @@ function parseSensors($csvFile) {
 
         while (($data = fgetcsv($handle)) !== FALSE) {
             $linea = trim($data[0]);
-            $sensore = trim($data[1]);
+            $sensoreRaw = trim($data[1]);
             $typeId = strtoupper(trim($data[2]));
             $descrizioneRaw = trim($data[3]);
 
-            $id = "L" . $linea . "S" . str_pad($sensore, 2, "0", STR_PAD_LEFT);
+            // Gestione sensori decimali (es. 1.1, 2.3)
+            $sensoreStr = str_pad(floor($sensoreRaw), 2, "0", STR_PAD_LEFT);
+            $sensore = (float)$sensoreRaw;
+            $sensoreId = strpos($sensoreRaw, '.') !== false ? "{$sensoreStr}." . explode('.', $sensoreRaw)[1] : $sensoreStr;
 
-            $key = (int)$linea * 1000 + (int)$sensore; // ordinamento per linea e sensore
+            $id = "L" . $linea . "S" . $sensoreId;
 
-            // Se già presente una riga valida, NON sovrascrivere con una NONE
+            // Chiave di ordinamento precisa, considera i decimali
+            $key = (int)$linea * 10000 + (float)($sensore * 10); // Moltiplico per 10 per gestire fino a .9
+
             if (isset($rows[$key])) {
                 if ($typeId !== 'NONE') {
                     $rows[$key] = [
@@ -38,7 +43,7 @@ function parseSensors($csvFile) {
         fclose($handle);
     }
 
-    // Ordina per chiave numerica (linea*1000 + sensore)
+    // Ordina per chiave numerica (linea*10000 + sensore*10)
     ksort($rows);
 
     // Rimuovi il flag interno e restituisci array [id => descrizione]
@@ -46,6 +51,5 @@ function parseSensors($csvFile) {
     foreach ($rows as $row) {
         $sensors[$row['id']] = $row['descrizione'];
     }
-
     return $sensors;
 }
