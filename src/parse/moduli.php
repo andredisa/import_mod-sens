862d85d6eb84f47ab2daf25a0f7c43d9d6256fbe
andredisa <andreadisanti22@gmail.com>
2025-04-28 08:37:29 +0200

    milgiorato il parse per i moduli, 1.1, 1.2 etc

--- src/parse/moduli.php
+++ src/parse/moduli.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../gestione/funzioni.php';
-
-function parseModules($csvFile) {
+function parseModules($csvFile)
+{
     $rows = [];
 
     if (($handle = fopen($csvFile, "r")) !== FALSE) {
@@ -9,20 +9,26 @@ function parseModules($csvFile) {
 
         while (($data = fgetcsv($handle)) !== FALSE) {
             $linea = trim($data[0]);
-            $modulo = trim($data[1]);
+            $moduloRaw = trim($data[1]);
             $typeId = strtoupper(trim($data[2]));
             $descrizioneRaw = trim($data[3]);
 
-            $id = "L" . $linea . "M" . str_pad($modulo, 2, "0", STR_PAD_LEFT);
+            // Gestione moduli decimali (es. 1.1, 2.3)
+            $moduloStr = str_pad(floor($moduloRaw), 2, "0", STR_PAD_LEFT);
+            $modulo = (float)$moduloRaw;
+            $moduloId = strpos($moduloRaw, '.') !== false ? "{$moduloStr}." . explode('.', $moduloRaw)[1] : $moduloStr;
 
-            $key = (int)$linea * 1000 + (int)$modulo; // ordinamento per linea e modulo
+            $id = "L" . $linea . "M" . $moduloId;
+
+            // Chiave di ordinamento precisa, considera i decimali
+            $key = (int)$linea * 10000 + (float)($modulo * 10); // Moltiplico per 10 per gestire max .9
 
             if (isset($rows[$key])) {
                 if ($typeId !== 'NONE') {
                     $rows[$key] = [
                         'id' => $id,
                         'descrizione' => cleanString($descrizioneRaw),
-                        'preferibile' => true
+                        'preferibile' => true 
                     ];
                 }
             } else {
@@ -37,7 +43,7 @@ function parseModules($csvFile) {
         fclose($handle);
     }
 
-    ksort($rows);
+    ksort($rows); // Ordina in base alla chiave
 
     $modules = [];
     foreach ($rows as $row) {
