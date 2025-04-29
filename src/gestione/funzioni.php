832d6b86c86b76d8c829e50477497b9135c870aa
andredisa <andreadisanti22@gmail.com>
2025-04-29 08:37:12 +0200

    migliroate le funzioni

--- src/gestione/funzioni.php
+++ src/gestione/funzioni.php
@@ -1,6 +1,5 @@
 <?php
-function cleanString($string)
-{
+function cleanString($string){
     // Rimuove caratteri invisibili e spazi Unicode strani
     $string = preg_replace('/[\x00-\x1F\x7F\xA0\x{200B}-\x{200D}\x{FEFF}]/u', '', $string); // invisibili e non-breaking
     $string = preg_replace('/\s+/', ' ', $string); // normalizza spazi
