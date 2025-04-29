2c069f0fbf95d500d4b80badaa4c64ec0e10b6be
andredisa <andreadisanti22@gmail.com>
2025-04-29 08:36:29 +0200

    cambio logica per il modifica excel

--- src/backEnd/modifica_excel.php
+++ src/backEnd/modifica_excel.php
@@ -188,7 +188,6 @@ if (
     echo "
     <!DOCTYPE html>
     <html lang='it'>
-
     <head>
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
@@ -199,7 +198,6 @@ if (
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet'>
         <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css' rel='stylesheet'>
     </head>
-
     <body>
             <div class='container'>
                 <h1>Operazione Completata</h1>
