c33075d46fa61671b271e56198a30827dd4da9e6
andredisa <andreadisanti22@gmail.com>
2025-04-30 08:38:27 +0200

    ottimizzato il parse dei sensori

--- src/parse/sensori.php
+++ src/parse/sensori.php
@@ -15,13 +15,13 @@ function parseSensors($csvFile)
 
             // Gestione sensori decimali (es. 1.1, 2.3)
             $sensoreStr = str_pad(floor($sensoreRaw), 2, "0", STR_PAD_LEFT);
-            $sensore = (float)$sensoreRaw;
+            $sensore = (float) $sensoreRaw;
             $sensoreId = strpos($sensoreRaw, '.') !== false ? "{$sensoreStr}." . explode('.', $sensoreRaw)[1] : $sensoreStr;
 
             $id = "L" . $linea . "S" . $sensoreId;
 
             // Chiave di ordinamento precisa, considera i decimali
-            $key = (int)$linea * 10000 + (float)($sensore * 10); // Moltiplico per 10 per gestire fino a .9
+            $key = (int) $linea * 10000 + (float) ($sensore * 10); // Moltiplico per 10 per gestire fino a .9
 
             if (isset($rows[$key])) {
                 if ($typeId !== 'NONE') {
@@ -50,6 +50,6 @@ function parseSensors($csvFile)
     foreach ($rows as $row) {
         $sensors[$row['id']] = $row['descrizione'];
     }
-    
+
     return $sensors;
 }
